5f736b194cf61ebfc1fba55c17fc390d4f38db1c
Soyebmahamud12 <96056076+Soyebmahamud12@users.noreply.github.com>
2021-12-13 17:38:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,26 @@
-# Resize the Window - Non Pep8 Compliant, mandated by Kivy
-from kivy.config import Config
-Config.set('graphics', 'width', '400')
-Config.set('graphics', 'height', '200')
-
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.properties import StringProperty
-
 import speech_recognition as sr
+import webbrowser
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+r3 = sr.Recognizer()
 
-r = sr.Recognizer()
-m = sr.Microphone()
+with sr.Microphone() as source:
+    print('[search edureka: search youtube]')
+    print('speak now')
+    audio = r3.listen(source)
 
+if 'edureka' in r2.recognize_google(audio):
+    r2 = sr.Recognizer()
+    url = 'https://www.edureka.co/'
+    with sr.Microphone() as source:
+        print('search your query')
+        audio = r2.listen(source)
 
-# Root Widget
-class Root(BoxLayout):
-    pass
-
-
-class RecordButton(Button):
-    # String Property to Hold output for publishing by Textinput
-    output = StringProperty('')
-    
-    def record(self):
-        # GUI Blocking Audio Capture
-        with m as source:
-            audio = r.listen(source)
-        
         try:
-            # recognize speech using Google Speech Recognition
-            value = r.recognize_google(audio)
-            self.output = "You said \"{}\"".format(value)
-        
-        except sr.UnknownValueError:
-            self.output = ("Oops! Didn't catch that")
-        
+            get = r2.recognize_google(audio)
+            print(get)
+            wb.get().open_new(url+get)
+        except sr.UnknownValueError as e:
+            print('error')
         except sr.RequestError as e:
-            self.output = ("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
-
-
-class SpeechApp(App):
-    def build(self):
-        # Calibrate the Microphone to Silent Levels
-        print("A moment of silence, please...")
-        with m as source:
-            r.adjust_for_ambient_noise(source)
-            print("Set minimum energy threshold to {}".format(r.energy_threshold))
-        # Create a root widget object and return as root
-        return Root()
-
-
-# When Executed from the command line (not imported as module), create a new SpeechApp
-if __name__ == '__main__':
-    SpeechApp().run()
-
+            print('failled'.format(e))
